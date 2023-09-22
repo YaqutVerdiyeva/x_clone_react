abcf0d703ed5a6312321c07e28abbb56ca2874bb
YaqutVerdiyeva <yaqutverdiyeva22@gmail.com>
2023-09-22 21:26:54 +0400

    add sidebar pages

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,9 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "~/layouts/main";
+import Bookmarks from "~/pages/bookmarks";
 import Explore from "~/pages/explore";
 import Home from "~/pages/home";
+import Lists from "~/pages/lists";
+import Messages from "~/pages/messages";
 import Notfound from "~/pages/not-found";
 import Notification from "~/pages/notifications";
+import Profile from "~/pages/profile";
 
 const routes = createBrowserRouter([
   {
@@ -21,6 +25,22 @@ const routes = createBrowserRouter([
       {
         path: "/notifications",
         element: <Notification />,
+      },
+      {
+        path: "/bookmarks",
+        element: <Bookmarks />,
+      },
+      {
+        path: "/lists",
+        element: <Lists />,
+      },
+      {
+        path: "/messages",
+        element: <Messages />,
+      },
+      {
+        path: "/profile",
+        element: <Profile />,
       },
       {
         path: "*",
